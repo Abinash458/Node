2cc11283f56a708d07612b64819a28262be2930c
Abinash Mohapatra <abinash@expensebit.com>
2020-12-25 13:53:41 +0530

    MVC implement

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 // app.engine('hbs', expressHbs({ layoutsDir: 'views/layout/', defaultLayout: 'main-layout', extname: 'hbs' }))
@@ -13,17 +15,15 @@ app.set('view engine', 'ejs');
 // app.set('view engine', 'pug');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: '404 | Page not found', path: '' })
-});
+app.use(errorController.get404Page);
 
 app.listen(3000);
